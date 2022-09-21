9c8dd0c15f9e7957c739c013c20a1f7283258fb2
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-09-21 13:46:38 -0300

    att 4.3.9 + AWS Bucket

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -16,5 +16,18 @@ module.exports = ({ env }) => ({
                 introspection: true,
             },
         }
+    },
+    upload: {
+        config: {
+            provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+            providerOptions: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+                region: env('AWS_REGION'),
+                params: {
+                    Bucket: env('AWS_BUCKET'),
+                },
+            },
+        },
     }
 });
