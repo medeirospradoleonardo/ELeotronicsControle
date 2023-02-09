2d7754843d0f3d037325ef5f44b7af8399de1ef6
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-09 19:37:51 -0300

    att

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-    proxy: true,
-    host: env('MY_URL'),
-    // url: env('MY_URL'),
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('MY_URL'), 
     app: {
         keys: env.array('APP_KEYS')
     },
