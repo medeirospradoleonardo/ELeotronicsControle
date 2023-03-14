ce303532321d15d80fa75006b4f44c4bb2b3d26f
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-03-14 04:52:07 -0300

    Att

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,21 @@
-module.exports = {
+module.exports = ({ env }) => ({
     // ...
     'produtos': {
       enabled: true,
       resolve: './src/plugins/produtos'
     },
+    upload: {
+      config: {
+          provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+          providerOptions: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                  Bucket: env('AWS_BUCKET'),
+              },
+          },
+      },
+    }
     // ...
-  }
+});
